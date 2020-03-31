723e742f631fe1932d26def772d13bf66eae05de
king <768334398@qq.com>
2020-03-31 12:45:22 +0800

    测试demo

---
+++ HelloReactor-master/src/main/java/testReactor.java
@@ -0,0 +1,9 @@
+
+public class testReactor {
+    public static void main(String[] args) {
+       BossGroup bossGroup = new BossGroup(8080);
+        Thread t = new Thread(bossGroup);
+        t.start();
+
+    }
+}
